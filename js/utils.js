c0a3ef13193560fea5b5a3ac69f222d0c526713b
Ilya Evtushenko <104614967+evt-front-dev@users.noreply.github.com>
2023-02-21 22:17:23 +0300

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,33 @@
+// функция перемешивания массива с иконками
+export const shuffle = (array) => {
+  let currentIndex = array.length,
+    randomIndex;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+};
+
+// функция задвоения массива
+export const duplicateArray = (array) => array.reduce((res, current) => res.concat([current, current]), []);
+
+// функция создания массива иконок
+export const createIconsArray = (initialCount) => {
+  const cardsIcons = ['camera-retro', 'cloud', 'play', 'bolt', 'stop', 'cogs', 'atom', 'basketball-ball'];
+
+  switch (initialCount) {
+    case 10:
+      return cardsIcons.slice(0, 5);
+    case 12:
+      return cardsIcons.slice(0, 6);
+    case 14:
+      return cardsIcons.slice(0, 7);
+    case 16:
+      return cardsIcons;
+    default:
+      break;
+  }
+};
